be4ef98e5b3ffee2e8798a2eb003db3a5e7603b2
Vincent Reydet <toubib.lag@gmail.com>
2021-02-14 08:11:16 +0100

    UQCRNFO

--- pingpong.py
+++ pingpong.py
@@ -82,7 +82,7 @@ while 1:
         canvas.create_text(200,200,text="Perdu !", font=('Courier', 50))
         tk.update_idletasks()
         tk.update()
-        time.sleep(10)
+        time.sleep(3)
         sys.exit()
     tk.update_idletasks()
     tk.update()
