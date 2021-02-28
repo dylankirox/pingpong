a26e6d8f2c9472556a56a99753fb3ac696adac6b
Dylan Reydet <dylankirox@gmail.com>
2021-02-28 11:59:09 +0100

    jldgkk

--- ajdhrqezu.py
+++ ajdhrqezu.py
@@ -7,4 +7,11 @@ class Jeu:
         self.tk = Tk()
         self.tk.title("M.Filiforme court vers la sortie")
         self.tk.resizable(0, 0)
-        self.tk.wm_attributes("-tomopost")
+        self.tk.wm_attributes("-tomopost", 1)
+        self.canvas = Canvas(self.tk, width=500, height=500, \
+            highlightthicknes=0)
+        self.canvas.pack()
+        self.tk.update()
+        self.hauteur_canevas = 500
+        self.largeur_canevas = 500
+        
