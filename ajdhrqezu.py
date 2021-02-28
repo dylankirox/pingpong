e00f6b46591be1cbc65d51fd5ec3a1bf3911c921
Dylan Reydet <dylankirox@gmail.com>
2021-02-28 14:11:24 +0100

    c

--- ajdhrqezu.py
+++ ajdhrqezu.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Canvas
+from tkinter import Tk, Canvas, PhotoImage
 import random
 import time
 
@@ -14,4 +14,30 @@ class Jeu:
         self.tk.update()
         self.hauteur_canevas = 500
         self.largeur_canevas = 500
-        
+        self.ap = PhotoImage(file="arrière_plan.gif")
+        larg = self.ap.width()
+        haut = self.ap.height()
+        for x in range(0, 5):
+            for y in range(0, 5):
+                self.canvas.create_image(x * larg, y * haut, \
+                    image=self.ap, anchor='nw')
+                self.lutins = []
+                self.enfonction = True
+
+    def boucle_principale(self):
+        while 1:
+            if self.enfonction == True:
+                for lutin in self.lutins:
+                    lutin.deplacer()
+                self.tk.update_idletasks()
+                self.tk.update()
+                time.sleep(0.01)
+class Coords:
+    def __init__(self, x1=0, y1=0, x2=0, y2=0):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+    def dans_x(co1, co2):
+        if (co1.x1 > co2.x1 and co1.x1 < co2.x2):
+            return True
